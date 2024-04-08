313456975621e470df02a191383041e5f316ebb9
Heder David <hedermd6@gmail.com>
2024-04-08 11:29:06 -0300

    Heap Minimo

---
+++ src/arvoreBinaria/heapMinimo/Amontoavel.kt
@@ -0,0 +1,16 @@
+package arvoreBinaria.heapMinimo
+
+interface Amontoavel {
+    fun inserir(dado: Int)
+    fun extrair(): Int?
+    fun obter(): Int?
+    fun atualizar(dado: Int)
+
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+    //private fun indicePai(filho: Int): Int
+    //private fun trocar(i: Int, j: Int)	//swap
+    //private fun ajustarAbaixo(pai: Int) 	//heapifyDown
+    //private fun ajustarAcima(indice: Int) //heapifyUp
+}
---
+++ src/arvoreBinaria/heapMinimo/Main.kt
@@ -0,0 +1,16 @@
+package arvoreBinaria.heapMinimo
+
+fun main() {
+    val heap: Amontoavel = ArvoreHeapMinimo()
+    heap.inserir(4)
+    heap.inserir(8)
+    heap.inserir(2)
+    heap.inserir(7)
+    heap.atualizar(6)
+    heap.inserir(1)
+    println(heap.imprimir())//[1,4,6,8,7]
+    println(heap.extrair())	//1
+    println(heap.obter())	//4
+    println(heap.obter())	//4
+    println(heap.imprimir())//[4,7,6,8]
+}
