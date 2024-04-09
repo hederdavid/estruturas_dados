bc556e50a1e602ebde2f45d68bc617f9e63a59a5
Heder David <hedermd6@gmail.com>
2024-04-09 10:04:32 -0300

    Heap Máximo

---
+++ src/arvoreBinaria/heapMaximo/Amontoavel.kt
@@ -0,0 +1,11 @@
+package arvoreBinaria.heapMaximo
+
+interface Amontoavel {
+    fun inserir(dado: Int)
+    fun extrair(): Int?
+    fun atualizar(dado: Int)
+    fun obter(): Int?
+    fun estaVazia(): Boolean
+    fun estaCheia(): Boolean
+    fun imprimir(): String
+}
