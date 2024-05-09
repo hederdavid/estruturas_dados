81e9ccbfc7373376d9d44e3cbbfae10dbd5c24a8
Heder David <hedermd6@gmail.com>
2024-05-08 21:29:26 -0300

    03 das questoes 2 e 3

---
+++ src/prova1aUnidadeComListaDinamica/N03/ListaDinamica.kt
@@ -0,0 +1,27 @@
+package prova1aUnidadeComListaDinamica.N03
+
+class ListaDinamica(var tamanho: Int = 10) {
+    private var ponteiroFim: NoDuplo? = null
+    private var ponteiroInicio: NoDuplo? = null
+    private var quantidade = 0
+
+    fun espiarInicio(): Any? {
+        var dadoEspiado: Any? = null
+        if (!estaVazia()) {
+            dadoEspiado = ponteiroInicio?.dado
+        } else {
+            println("Fila Vazia!")
+        }
+        return dadoEspiado
+    }
+
+
+
+    fun estaCheia(): Boolean {
+        return quantidade == tamanho
+    }
+
+    fun estaVazia(): Boolean {
+        return quantidade == 0
+    }
+}
