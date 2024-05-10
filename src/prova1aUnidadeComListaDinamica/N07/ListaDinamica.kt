6ee5cc8cfcdb189efd49634cd4d5042e774da796
Heder David <hedermd6@gmail.com>
2024-05-09 21:07:48 -0300

    07 das questoes 2 e 3

---
+++ src/prova1aUnidadeComListaDinamica/N07/ListaDinamica.kt
@@ -0,0 +1,52 @@
+package prova1aUnidadeComListaDinamica.N07
+
+class ListaDinamica(var tamanho: Int = 10) {
+    private var ponteiroFim: NoDuplo? = null
+    private var ponteiroInicio: NoDuplo? = null
+    private var quantidade = 0
+
+    fun verificarPosicaoPrimeiraOcorrencia(dado: Any?): Any? {
+        var posicao: Any? = "Elemento não encontrado na lista"
+        var ponteiroAuxiliar = ponteiroInicio
+        if (!estaVazia()) {
+            for (i in 0 ..< quantidade) {
+                if (ponteiroAuxiliar?.dado == dado) {
+                    posicao = i
+                    break
+                } else {
+                    ponteiroAuxiliar = ponteiroAuxiliar?.proximo
+                }
+            }
+        } else {
+            println("Fila Vazia!")
+        }
+        return posicao
+    }
+
+    //Apenas pra teste
+    fun inserirFim(dado: Any?) {
+        if (!estaCheia()) {
+            var noTemp: NoDuplo? = NoDuplo(dado)
+            noTemp?.anterior = ponteiroFim
+            if (!estaVazia()) {
+                ponteiroFim?.proximo = noTemp
+            } else {
+                ponteiroInicio = noTemp
+            }
+            ponteiroFim = noTemp
+            quantidade++
+        } else {
+            println("Lista Cheia!")
+        }
+    }
+
+
+
+    fun estaCheia(): Boolean {
+        return quantidade == tamanho
+    }
+
+    fun estaVazia(): Boolean {
+        return quantidade == 0
+    }
+}
---
+++ src/prova1aUnidadeComListaDinamica/N07/Main.kt
@@ -0,0 +1,12 @@
+package prova1aUnidadeComListaDinamica.N07
+
+fun main() {
+    var teste: ListaDinamica = ListaDinamica();
+    teste.inserirFim("teste")
+    teste.inserirFim(0)
+    teste.inserirFim(2)
+    teste.inserirFim("teste")
+    teste.inserirFim(0)
+    println(teste.verificarPosicaoPrimeiraOcorrencia("teste"))
+    println(teste.verificarPosicaoPrimeiraOcorrencia(0))
+}
